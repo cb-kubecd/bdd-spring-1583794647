04a45d304e46140e951da33728c0cf402d94e904
jenkins-x-bot-test <jenkins-x-test@googlegroups.com>
2020-03-09 22:57:29 +0000

    Initial import

---
+++ src/test/java/com/example/bddspring1583794647/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bddspring1583794647;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
